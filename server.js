53edfdb873fef46be562d8bc0a5d20a76ccd01e8
Glad <94646897+ovoglad@users.noreply.github.com>
2023-06-15 12:11:33 -0400

    added form

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const path = require("path");
+const data = require("./modules/officeData.js");
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/contactus", (req, res) => {
+  res.sendFile(__dirname + "/views/form.html");
+});
+
+app.post("/contactus", (req, res) => {
+  const formData = req.body;
+
+  console.log(formData);
+
+  res.send("Thank you for contacting us!");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
@@ -22,12 +36,38 @@ app.get("/list", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/list.html"));
 });
 
+app.get("/form", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/form.html"));
+});
+
+app.get("/employee/:employeeNum", (req, res) => {
+  data
+    .getEmployeeByNum(req.params.employeeNum)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: "no results" });
+    });
+});
+
+app.get("/PartTimer", (req, res) => {
+  data.getPartTimers().then((data) => {
+    res.json(data);
+  });
+});
+
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
 });
 
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(HTTP_PORT, () => {
-  console.log("server listening on port: " + HTTP_PORT);
-});
+data
+  .initialize()
+  .then(function () {
+    app.listen(HTTP_PORT, () => {
+      console.log("server listening on port: " + HTTP_PORT);
+    });
+  })
+  .catch(function (err) {
+    console.log("unable to start server: " + err);
+  });
